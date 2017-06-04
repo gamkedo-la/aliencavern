90fb891ba7a42398e50e6a03c58dc8ae0406853f
Artur Slomowski <artur.slomowski@gmail.com>
2017-06-04 22:58:03 +0200

    Level editor: change tile on click

--- js/level_editor/level_editor.js
+++ js/level_editor/level_editor.js
@@ -1,6 +1,6 @@
-var heightlight_row_x;
-var heightlight_column_y;
-
+var current_row;
+var current_column;
+var mouse_up = true;
 function levelEditorInitialization() {
     colorRect(0,0, screen.width,screen.height, 'black');
     setDefaultCavern();
@@ -8,9 +8,14 @@ function levelEditorInitialization() {
 }
 
 function drawLevelEditor() {
+    loadGameObjects(aliens, alienPic ,ALIEN);
+    loadGameObjects(alienPlants, alienPlantPic, ALIEN_PLANT);
+    loadGameObjects(crew, crewPic, CREW);
+    loadGameObjects(shipParts, shipPartPic, SHIP_PART);
     drawOnlyCavernOnScreen();
     highlightTile();
-    drawHighlightRect(heightlight_row_x, heightlight_column_y);
+    drawHighlightRect(current_row, current_column);
+
 }
 
 function setCamera() {
@@ -40,9 +45,32 @@ function highlightTile() {
         var tile_x = Math.floor(mousePosition.x / BRICK_W);
         var tile_y = Math.floor(mousePosition.y / BRICK_H);
 
-        heightlight_row_x = tile_x * BRICK_W;
-        heightlight_column_y = tile_y * BRICK_H;
+        current_row = tile_x * BRICK_W;
+        current_column= tile_y * BRICK_H;
     });
+
+    canvas.addEventListener('mousedown', function(evt){
+        if(mouse_up) {
+            mouse_up = false;
+            change_tile();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt){
+            mouse_up = true;
+    });
+}
+
+function change_tile() {
+    var tileCol = current_column / BRICK_H;
+    var tileRow = current_row / BRICK_W;
+
+    var brickIndex = brickTileToIndex(tileRow, tileCol);
+    if(cavernGrid[brickIndex] < 8) {
+        cavernGrid[brickIndex] = cavernGrid[brickIndex] + 1;
+    } else {
+        cavernGrid[brickIndex] = 0;
+    }
 }
 
 function getMousePosition(canvas, evt) {
@@ -58,3 +86,18 @@ function drawHighlightRect(x, y) {
     canvasContext.lineWidth=2;
     canvasContext.strokeRect(x, y, BRICK_W, BRICK_H);
 }
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
