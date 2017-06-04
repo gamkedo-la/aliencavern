dbeb0ca740cb9c23b0360b0588f93e7cfedb87ec
Artur Slomowski <artur.slomowski@gmail.com>
2017-06-04 22:25:43 +0200

    Level editor: highlight current working tile

--- js/level_editor/level_editor.js
+++ js/level_editor/level_editor.js
@@ -1,3 +1,6 @@
+var heightlight_row_x;
+var heightlight_column_y;
+
 function levelEditorInitialization() {
     colorRect(0,0, screen.width,screen.height, 'black');
     setDefaultCavern();
@@ -6,6 +9,8 @@ function levelEditorInitialization() {
 
 function drawLevelEditor() {
     drawOnlyCavernOnScreen();
+    highlightTile();
+    drawHighlightRect(heightlight_row_x, heightlight_column_y);
 }
 
 function setCamera() {
@@ -27,4 +32,29 @@ function drawBoundries() {
     }
 
     cavernGrid = cavern;
+}
+
+function highlightTile() {
+    canvas.addEventListener('mousemove', function(evt){
+        mousePosition = getMousePosition(canvas, evt);
+        var tile_x = Math.floor(mousePosition.x / BRICK_W);
+        var tile_y = Math.floor(mousePosition.y / BRICK_H);
+
+        heightlight_row_x = tile_x * BRICK_W;
+        heightlight_column_y = tile_y * BRICK_H;
+    });
+}
+
+function getMousePosition(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+}
+
+function drawHighlightRect(x, y) {
+    canvasContext.strokeStyle='#FFFF00';
+    canvasContext.lineWidth=2;
+    canvasContext.strokeRect(x, y, BRICK_W, BRICK_H);
 }
