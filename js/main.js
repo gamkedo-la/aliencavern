7eb300a1483a80e94f4d681a6fda98d8ddfde39b
Christer Kaitila <github@mcfunkypants.com>
2017-06-28 13:49:39 -0700

    health bar

--- js/main.js
+++ js/main.js
@@ -63,7 +63,6 @@ function updateAll() {
     }
 }
 
-
 function drawAll() {
     // drawing black to erase previous frame, doing before .translate() since
     // its coordinates are not supposed to scroll when the camera view does
@@ -105,5 +104,10 @@ function drawAll() {
     //canvasContext.fillText("Arrow keys to slide, scrolling demo",8,14);
 
     //debug shootProjectile(200, 30, canvas.width / 2, canvas.height / 2, 20, 2, 5, 100);
+
+    // draw the health bar
+    colorRect(10,canvas.height-40,canvas.width-20,30,"rgba(66,00,00,0.4)"); // border
+    colorRect(15,canvas.height-35,(canvas.width-30)*(playerHealth/MAX_HEALTH),20,"rgba(255,00,00,0.4)"); // health
+
 
 }
